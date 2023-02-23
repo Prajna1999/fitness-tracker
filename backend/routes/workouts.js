6d25dce6ca745869e18f37b6be1f0f97dcbb4421
Prajna1999 <gituprajna20@gmail.com>
2023-02-23 16:28:56 +0530

    add: mongoose ODm

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -7,15 +7,33 @@ const router=express.Router()
 // /api/workouts/ relative path
 router.get('/', (req,res)=>{
 
-    res.json({mssg:"It works"})
+    res.json({mssg:"GET All workouts"})
     // console.log("it works")
 })
+//GET a single workout
+router.get('/:id', (req,res)=>{
+    res.json({mssg:'GET a single workout'})
+})
 
+//create a single workout doc
+router.post('/', (req,res)=>{
+    res.json({mssg:'POST a single workout'})
+})
+
+//delete a single workout.
+router.delete('/:id', (req,res)=>{
+    res.json({mssg:'DELETE a single workout.'})
+})
+
+//update a single workout.
+router.patch('/:id', (req,res)=>{
+    res.json({mssg: 'UPDATE a single workout'})
+})
 //this fires when we make a request to
 // /api/workouts/hello relative path.
-router.get('/test', (req,res)=>{
-    res.json({mssg:"This too"})
-})
+// router.get('/test', (req,res)=>{
+//     res.json({mssg:"This too"})
+// })
 
 
 module.exports=router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express=require('express')
-
+const mongoose=require('mongoose');
 const workoutRoutes=require('./routes/workouts')
 
 require('dotenv').config()
@@ -13,6 +13,8 @@ const PORT=5001||process.env.PORT
 //express app
 const app=express()
 
+//middle ware for POST requests handling
+app.use(express.json())
 
 //middleware, logging the path everytime
 //client makes a request on the FE
@@ -24,9 +26,17 @@ app.use((req,res,next)=>{
 //GET request handler
 app.use('/api/workouts',workoutRoutes)
 
+//connect to DB
 
-//listen to a certain port number
+mongoose.set('strictQuery', true)
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        //listen to a certain port number
 
 app.listen(PORT, ()=>{
     console.log('listening on port',PORT)
 })
+    }).catch((error)=>{
+        console.log(error)
+    })
+
